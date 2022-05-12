0c75be3489d718585ff82ed74b847ca1fc8bf93f
Savita Verma <svitaverma10@gmail.com>
2022-05-12 13:21:02 +0530

    feat: add signup screen

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { Routes, Route } from "react-router-dom";
-import { Login } from "./pages";
+import { Login, Signup } from "./pages";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/login" element={<Login />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
       </Routes>
     </div>
   );
